a34f4bdbec168a7b14aa023417731dd724e737ec
leila7575 <leila.louajri@gmail.com>
2024-01-09 02:30:10 -0800

    adding inserting left-child node function file

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -22,6 +22,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent->left != NULL)
 	{
+		free(parent->left);
 		new_node->left = parent->left;
 		new_node->left->parent = new_node;
 	}
