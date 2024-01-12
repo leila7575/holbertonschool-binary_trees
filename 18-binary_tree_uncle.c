d8a878ced4ab1464f714f134aab04c71e9666cb3
leila7575 <leila.louajri@gmail.com>
2024-01-12 18:24:41 +0100

    adding binary_tree_uncle function file

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,9 +9,9 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
-	if (node->parent->parent->left == node)
+	if (node->parent->parent->left == node->parent)
 		return (node->parent->parent->right);
-	if (node->parent->parent->right == node)
+	if (node->parent->parent->right == node->parent)
 		return (node->parent->parent->left);
 	return (NULL);
 }
